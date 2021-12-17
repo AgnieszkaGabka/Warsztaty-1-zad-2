4ee9963104b90b18749ab2c0a64b12b3558b1949
AgnieszkaGabka <94054240+AgnieszkaGabka@users.noreply.github.com>
2021-12-17 18:56:00 +0100

    Add files via upload
    code changed to english according to instruction in task description.

--- Lotto.py
+++ Lotto.py
@@ -1,4 +1,4 @@
-#Napisz program, który:
+#Program, który:
 #zapyta o typowane liczby, przy okazji sprawdzi następujące warunki:
         #czy wprowadzony ciąg znaków jest poprawną liczbą,
         #czy użytkownik nie wpisał tej liczby już poprzednio,
@@ -9,65 +9,65 @@
 
 import random
 
-def lotek():
-    lista = []
+def lotto():
+    list = []
     try:
-        a = int(input("Podaj pierwszą liczbę: ")) #poproś o wpisanie liczby
+        a = int(input("Enter the first number: ")) #poproś o wpisanie liczby
         if a in range (1, 50):
-            lista.append(a)
+            list.append(a)
         else:
-            print("Podaj numer z zakresu od 1 do 49!") #jeśli liczba w przedziale 1-50, dodaj do listy lista, jeśli nie - zwróć odpowiedź
-        b = int(input("Podaj drugą liczbę: "))
-        if b in range(1, 50) and b not in lista:
-            lista.append(b) #jeśli gracz nie wybierał wcześniej takiej liczby, dodaj ją do listy
+            print("Enter a number between 1 and 49!") #jeśli liczba w przedziale 1-50, dodaj do listy lista, jeśli nie - zwróć odpowiedź
+        b = int(input("Enter the second number: "))
+        if b in range(1, 50) and b not in list:
+            list.append(b) #jeśli gracz nie wybierał wcześniej takiej liczby, dodaj ją do listy
         else:
-            print("Podaj numer z zakresu od 1 do 49, liczby nie mogą się powtarzać!") #jeśli dane nie spełnią powyższych warunków, wyświetl odpowiedż
-        c = int(input("Podaj trzecią liczbę: "))
-        if c in range(1, 50) and c not in lista:
-            lista.append(c) # jeśli gracz nie wybierał wcześniej takiej liczby, dodaj ją do listy
+            print("Enter a number from 1 to 49, numbers cannot be repeated!") #jeśli dane nie spełnią powyższych warunków, wyświetl odpowiedż
+        c = int(input("Enter the third number: "))
+        if c in range(1, 50) and c not in list:
+            list.append(c) # jeśli gracz nie wybierał wcześniej takiej liczby, dodaj ją do listy
         else:
-            print("Podaj numer z zakresu od 1 do 49, numery nie mogą się powtarzać!") #jeśli dane nie spełnią powyższych warunków, wyświetl odpowiedż
-        d = int(input("Podaj czwartą liczbę: "))
-        if d in range(1, 50) and d not in lista:
-            lista.append(d) #jeśli gracz nie wybierał wcześniej takiej liczby, dodaj ją do listy
+            print("Enter a number from 1 to 49, numbers cannot be repeated!") #jeśli dane nie spełnią powyższych warunków, wyświetl odpowiedż
+        d = int(input("Enter the fourth number: "))
+        if d in range(1, 50) and d not in list:
+            list.append(d) #jeśli gracz nie wybierał wcześniej takiej liczby, dodaj ją do listy
         else:
-            print("Podaj numer z zakresu od 1 do 49, liczby nie mogą się powtarzać!") #jeśli dane nie spełnią powyższych warunków, wyświetl odpowiedż
-        e = int(input("Podaj piątą liczbę: "))
-        if e in range(1, 50) and e not in lista:
-            lista.append(e) #jeśli gracz nie wybierał wcześniej takiej liczby, dodaj ją do listy
+            print("Enter a number from 1 to 49, numbers cannot be repeated!") #jeśli dane nie spełnią powyższych warunków, wyświetl odpowiedż
+        e = int(input("Enter the fifth number: "))
+        if e in range(1, 50) and e not in list:
+            list.append(e) #jeśli gracz nie wybierał wcześniej takiej liczby, dodaj ją do listy
         else:
-            print("Podaj numer z zakresu od 1 do 49, liczby nie mogą się powtarzać!") #jeśli dane nie spełnią powyższych warunków, wyświetl odpowiedż
-        f = int(input("Podaj szóstą liczbę: "))
-        if f in range(1, 50) and f not in lista:
-            lista.append(f) #jeśli gracz nie wybierał wcześniej takiej liczby, dodaj ją do listy
+            print("Enter a number from 1 to 49, numbers cannot be repeated!") #jeśli dane nie spełnią powyższych warunków, wyświetl odpowiedż
+        f = int(input("Enter the sixth number: "))
+        if f in range(1, 50) and f not in list:
+            list.append(f) #jeśli gracz nie wybierał wcześniej takiej liczby, dodaj ją do listy
         else:
-            print("Podaj numer z zakresu od 1 do 49, liczby nie mogą się powtarzać!") #jeśli dane nie spełnią powyższych warunków, wyświetl odpowiedż
+            print("Enter a number from 1 to 49, numbers cannot be repeated!") #jeśli dane nie spełnią powyższych warunków, wyświetl odpowiedż
     except (TypeError, ValueError):
-        print("To nie jest liczba") #jeśli liczba nie spełni żadnych powyższych warunków, wyświetl odpowiedż
-    lista.sort()
-    print(f"Wybrałeś następujące liczby: {lista}") #wyświetl wszystkie wybrane przez gracza liczby
+        print("This is not a number") #jeśli liczba nie spełni żadnych powyższych warunków, wyświetl odpowiedż
+    list.sort()
+    print(f"You chose the following numbers: {list}") #wyświetl wszystkie wybrane przez gracza liczby
     if len(lista) < 6: #jeśli gracz nie wprowadzi 6 liczb spełniajacych powyższe warunki, wyświetl informację
-        print("Spróbuj ponownie - wprowadź 6 liczb w zakresie od 1 do 49, liczby nie mogą się powtarzać")
-    return(lista)
+        print("Try again - enter 6 numbers between 1 and 49, numbers cannot be repeated")
+    return(list)
 
-def losowanie():
-    losy = []
+def lottery():
+    lots = []
     for n in range (1,7): #losowanie komputera - sześciokrotne wybranie losowej liczby z zakresu od 1 do 50
         n = random.randint(1, 50)
-        losy.append(n)
-    losy.sort()
-    print(f"Komputer wylosował: {losy}")
-    return(losy) #funkcja zwraca listę wybranych przez komputer liczb
+        lots.append(n)
+    lots.sort()
+    print(f"Computer drew: {lots}")
+    return(lots) #funkcja zwraca listę wybranych przez komputer liczb
 
 
-def porownanie(): #funkcja porównująca wylosowane przez gracza i przez komputer liczby
-    a = lotek()
+def comparison(): #funkcja porównująca wylosowane przez gracza i przez komputer liczby
+    a = lottery()
     b = losowanie()
     c = [i for i in a if i in b]
     hits = 0
     for i in c: #funkcja licząca, ile liczb powtarza się w obu listach - gracza i komputera
             hits += 1
-    print(f"Udało Ci się trafić {hits} {'liczbę' if hits == 1 else 'liczb'}!")
+    print(f"You hit {hits}!")
 
 
-print(porownanie())
+print(comparison())
